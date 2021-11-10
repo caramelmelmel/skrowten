97bade9f997aeb2513cd3d90c136c20393a6b4c8
jeanmah <jeanmah.828@gmail.com>
2021-11-10 22:59:39 +0800

    using 1 type of throttling

--- auto_test_multiple_sitespeed.sh
+++ auto_test_multiple_sitespeed.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo 'Stopping Docker networks'
 docker network rm 3g
-docker network rm 3gslow
 
 echo 'Starting Docker networks'
 docker network create --driver bridge --subnet=192.168.33.0/24 --gateway=192.168.33.10 --opt "com.docker.network.bridge.name"="docker1" 3g
@@ -9,36 +8,23 @@ tc qdisc add dev docker1 root handle 1: htb default 12
 tc class add dev docker1 parent 1:1 classid 1:12 htb rate 1.6mbit ceil 1.6mbit
 tc qdisc add dev docker1 parent 1:12 netem delay 150ms
 
-docker network create --driver bridge --subnet=192.168.36.0/24 --gateway=192.168.36.10 --opt "com.docker.network.bridge.name"="docker4" 3gslow
-tc qdisc add dev docker4 root handle 1: htb default 12
-tc class add dev docker4 parent 1:1 classid 1:12 htb rate 0.4mbit ceil 0.4mbit
-tc qdisc add dev docker4 parent 1:12 netem delay 200ms
-
 echo 'Browsertime Running 3g and 3g slow'
 http3WebsitesFile="./testing_sites.txt"
+outputFolder = "testingOutput"
 
 counter=1
 
 while IFS= read -r site
 do
 	echo "--------- $site ------------"
-	docker run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:11.0.0-plus1 --network=3g -c 3g --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ $site --plugins.add analysisstorer --plugins.add /lighthouse
+	docker run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:11.0.0-plus1 --network=3g -c 3g --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ --outputFolder '$outputFolder' $site --plugins.add analysisstorer --plugins.add /lighthouse
 
-	echo "-----Done $counter 3g HTTP2-----"
+	echo "-----Done $counter HTTP2-----"
 
-	docker run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:11.0.0-plus1 --network=3gslow -c 3gslow --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ $site --plugins.add analysisstorer --plugins.add /lighthouse
-
-	echo "-----Done $counter 3gslow HTTP2-----"
-
-	
-	docker run --rm -u $USER -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:20.5.0-plus1 --network=3g -c 3g --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ $site --plugins.add analysisstorer --plugins.add /lighthouse
+	docker run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:20.5.0-plus1 --network=3g -c 3g --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ --outputFolder '$outputFolder' $site --plugins.add analysisstorer --plugins.add /lighthouse
 
 
-	echo "-----Done $counter 3g HTTP3-----"
-
-	docker run --rm -u $USER -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:20.5.0-plus1 --network=3gslow -c 3gslow --iterations 3 --prettyPrint true --cacheClearRaw true --preURL https://www.lazada.sg/ $site --plugins.add analysisstorer --plugins.add /lighthouse
-
-	echo "-----Done $counter 3gslow HTTP3-----"
+	echo "-----Done $counter HTTP3-----"
 
 	((counter=counter+1))
 
